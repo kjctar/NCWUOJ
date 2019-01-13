20a849647d5b83cfa294389349c4ce5e0a07c86e
kjctar <2412864341@qq.com>
2019-01-13 22:04:33 +0800

    update

--- web/discuss3/wang/upload.php
+++
@@ -1,5 +0,0 @@
-<?php
- header("charset=utf-8"); 
-$ret=array("errno" => 0,"data"=>["1.jpg","1.jpg","1.jpg"]);
-echo json_encode($ret);
-?>
--- web/logout.php
+++
@@ -1,6 +0,0 @@
-<?php session_start();
-unset($_SESSION[$OJ_NAME.'_'.'user_id']);
-session_destroy();
-echo'<script> parent.location.href="index.php?'.rand().'"   </script>';
-//header("Location:../../index.php");
-?>
